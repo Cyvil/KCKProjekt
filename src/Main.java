1620dc05bbd5d20c9cb18a4eaccd410aff948cd6
golcia <martyna_gola@wp.pl>
2014-11-04 22:18:20 +0100

    Revert "Graphic representation"
    This reverts commit 993c7e3bac7e2119d4ab3a3451a0e8d709bab751.

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,13 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		//generowanie landmarkow
-		/*Environment environment = new Environment();		
+		// TODO Auto-generated method stub
+		Environment environment = new Environment();
+		
 		for (int i = 0; i < environment.landmarkArraySize; i++)
 		{
 			System.out.println("Landmark " + i + ": " + environment.landmarkArray[i].getType() +" pozycja : x = " + environment.landmarkArray[i].getPoint().getX() + " y = "  + environment.landmarkArray[i].getPoint().getY()  +  " widocznoœæ :" + environment.landmarkArray[i].getRadius(false) + " Kolizja :" + environment.landmarkArray[i].getRadius(true));
 		}
-		*/
-		//tworzenie okna
-		Window Okno=new Window();
-		
-				
 	}
 
-	
 }
